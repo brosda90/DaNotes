554f1d8a0da863075309fd1e90b4a79b2ba7d17c
brosda90 <140012668+brosda90@users.noreply.github.com>
2023-12-07 22:27:05 +0100

    first commit

---
+++ src/app/firebase-services/note-list.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  doc,
+  collectionData,
+  onSnapshot,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubTrash;
+  unsubNotes;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+  }
+
+  ngOnDestroy() {
+    this.unsubTrash();
+    this.unsubNotes();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach((element) => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach((element) => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    };
+  }
+
+  //const itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'notepad-cfd10',
+    appId: '1:743878296666:web:3ed6a667e1c2554f908463',
+    storageBucket: 'notepad-cfd10.appspot.com',
+    apiKey: 'AIzaSyD1Hx4cFBOC8jZNc6JSYzIZPPyw68Vw-Dk',
+    authDomain: 'notepad-cfd10.firebaseapp.com',
+    messagingSenderId: '743878296666',
+  },
+};
+
+//"projectId": "notepad-cfd10",
+//"appId": "1:743878296666:web:3ed6a667e1c2554f908463",
+//"storageBucket": "notepad-cfd10.appspot.com",
+//"apiKey": "AIzaSyD1Hx4cFBOC8jZNc6JSYzIZPPyw68Vw-Dk",
+//"authDomain": "notepad-cfd10.firebaseapp.com",
+//"messagingSenderId": "743878296666"
